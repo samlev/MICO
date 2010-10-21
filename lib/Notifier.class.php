29ceb496d02976053e5d241421ac2ce424b91134
Samuel Levy <sam@samuellevy.com>
2010-10-21 22:52:51 +1000

    Added the notifier class

---
+++ lib/Notifier.class.php
@@ -0,0 +1,19 @@
+<?php
+/*******************************************************************************
+ *******************************************************************************
+ ** Author: Samuel Levy <sam@samuellevy.com>
+ ** 
+ ** File: lib/Notifier.class.php
+ ** 
+ ** Description: Defines a simple class to encapsulate the Notification
+ **              functionality
+ *******************************************************************************
+ ******************************************************************************/
+
+class Notifier {
+    /** Builds and sends all notification emails */
+    static function run() {
+        
+    }
+}
+?>
