f943049fe27343682576c71001e6840ab8884657
Samuel Levy <sam@samuellevy.com>
2011-05-06 19:58:58 +1000

    Added upgrade for 0.13.4 beta

--- upgrade/api/upgrade.php
+++ upgrade/api/upgrade.php
@@ -37,7 +37,8 @@ $versions = array('0.11.1b',
                   '0.13.0a',
                   '0.13.1a',
                   '0.13.2a',
-                  '0.13.3a');
+                  '0.13.3a',
+                  '0.13.4b');
 
 // get the old and new versions
 $old = Settings::get_default('MICO_VERSION','unknown');
